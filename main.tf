f523835fd5d273f9aa35d36dc5ab824b349c0847
venna12 <vmrreddy913@gmail.com>
2023-04-20 16:27:53 +0530

    bcnsab

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIAZQM33DKB53ZNPBGI"
+secret_key = "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"
+}
+
+resource "aws_instance" "web" {
+ami     = "ami-007855ac798b5175e"
+instance_type = "t2.micro"
+count         =   3
+key_name     = "11amclass"
+tags  =  {
+Name  = "terraform"
+}
+}
